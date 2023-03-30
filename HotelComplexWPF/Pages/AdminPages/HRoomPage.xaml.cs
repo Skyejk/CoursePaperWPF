8c2b1b6c1e2b18b7f17b420d03b9b685baf63be9
mitya_tihiy <mitya_tihiy@mail.ru>
2023-03-30 15:09:09 +0300

    added admin pages

---
+++ HotelComplexWPF/Pages/AdminPages/HRoomPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelComplexWPF.Pages.AdminPages
+{
+    /// <summary>
+    /// Логика взаимодействия для HRoomPage.xaml
+    /// </summary>
+    public partial class HRoomPage : Page
+    {
+        public HRoomPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
