c37a74162953e457d361ab8be52ca76c928baca1
mitya_tihiy <mitya_tihiy@mail.ru>
2023-04-05 14:19:39 +0300

    added CountryPages & RegionPages

---
+++ HotelComplexWPF/Pages/AdminPages/AddEditPages/AddEditCountryPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using HotelComplexWPF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelComplexWPF.Pages.AdminPages.AddEditPages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditCountryPage.xaml
+    /// </summary>
+    public partial class AddEditCountryPage : Page
+    {
+        private Country currentCountry = new Country();
+        public AddEditCountryPage(Country selectedCountry)
+        {
+            InitializeComponent();
+
+            if (selectedCountry != null)
+                currentCountry = selectedCountry;
+
+            DataContext = currentCountry;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(currentCountry.CodeCountry))
+                errors.AppendLine("Укажите код страны");
+            if (string.IsNullOrWhiteSpace(currentCountry.ShortName))
+                errors.AppendLine("Укажите название страны");
+            if (string.IsNullOrWhiteSpace(currentCountry.FullName))
+                errors.AppendLine("Укажите полное название страны");
+            if (string.IsNullOrWhiteSpace(currentCountry.FlagCountry))
+                errors.AppendLine("Выберите флаг");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (currentCountry.ID == 0)
+                HotelEntities.GetContext().Country.Add(currentCountry);
+
+            try
+            {
+                HotelEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+                NavigationService.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+    }
+}
--- HotelComplexWPF/Pages/AdminPages/RegionPage.xaml.cs
+++ HotelComplexWPF/Pages/AdminPages/RegionPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HotelComplexWPF.Entities;
+using HotelComplexWPF.Pages.AdminPages.AddEditPages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,47 @@ namespace HotelComplexWPF.Pages.AdminPages
         public RegionPage()
         {
             InitializeComponent();
+        }
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddEditRegionPage((sender as Button).DataContext as Region));
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var regionForRemoving = DGridRegion.SelectedItems.Cast<Region>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {regionForRemoving.Count()} элементов?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    HotelEntities.GetContext().Region.RemoveRange(regionForRemoving);
+                    HotelEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+
+                    DGridRegion.ItemsSource = HotelEntities.GetContext().Region.ToList();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddEditRegionPage(null));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                HotelEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridRegion.ItemsSource = HotelEntities.GetContext().Region.ToList();
+            }
         }
     }
 }
