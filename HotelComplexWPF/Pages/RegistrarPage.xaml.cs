1bb31661a33b8403c59d64732cd39907653ffe7f
mitya_tihiy <mitya_tihiy@mail.ru>
2023-04-06 16:48:11 +0300

    added a page for registrars

--- HotelComplexWPF/Pages/RegistrarPage.xaml.cs
+++ HotelComplexWPF/Pages/RegistrarPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using HotelComplexWPF.Entities;
+using HotelComplexWPF.Pages.RegistrarPages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,32 @@ namespace HotelComplexWPF.Pages
         public RegistrarPage(User currentUser)
         {
             InitializeComponent();
+
+            string fullNameThisPerson =
+                $"{currentUser.Employee.DetailedInformationAboutThePerson.Human.Surname} " +
+                $"{currentUser.Employee.DetailedInformationAboutThePerson.Human.Firstname} " +
+                $"{currentUser.Employee.DetailedInformationAboutThePerson.Patronymic}";
+
+            tblFIO.Text = fullNameThisPerson;
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                HotelEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridReservationHRoom.ItemsSource = HotelEntities.GetContext().ReservationHotelRoom.ToList();
+            }
+        }
+
+        private void btnAddReservation_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddReservation(null));
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddReservation((sender as Button).DataContext as ReservationHotelRoom));
         }
     }
 }
